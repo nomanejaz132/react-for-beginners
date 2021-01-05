f906e7b17c5a25a2dbf3549616b931092a0b1e28
Noman Ejaz <nomanejaz132@gmail.com>
2021-01-05 23:47:27 +0500

    First Commit

---
+++ src/components/FunctionalComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FunctionalComponent() {
+    return (
+        <div>
+            This is pure react functional component.
+        </div>
+    )
+}
+
+export default FunctionalComponent
